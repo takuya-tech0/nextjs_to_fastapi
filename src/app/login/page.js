821d9e5ff7088dbe1d87bc6672bdaa9d53fe33ba
takuya-tech0 <140943306+takuya-tech0@users.noreply.github.com>
2024-10-22 01:44:49 +0900

    Update page.js

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -10,7 +10,7 @@ export default function Login() {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch('https://tech0-gen-7-step3-studentwebapp-pos-test2-ekh7bbekctbtbee8.eastus-01.azurewebsites.net/login', {
+      const response = await fetch('${process.env.NEXT_PUBLIC_API_URL}/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
