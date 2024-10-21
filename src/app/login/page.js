4e8e8da52800aca81e72ef6f3b890d0be1a4cf12
takuya-tech0 <140943306+takuya-tech0@users.noreply.github.com>
2024-10-22 01:50:59 +0900

    Update page.js

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -10,8 +10,8 @@ export default function Login() {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch('${process.env.NEXT_PUBLIC_API_URL}/login', {
-        method: 'POST',
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/login`, {
+        method: 'POST',             
         headers: {
           'Content-Type': 'application/json',
         },
